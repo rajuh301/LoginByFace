774b297dbc79f3de26b85e1cc326cc38a7081d32
Raju hassan <rajuh301@gmail.com>
2025-05-30 15:02:13 +0600

    all Update

--- src/components/FaceLogin.tsx
+++ src/components/FaceLogin.tsx
@@ -61,7 +61,8 @@ export default function FaceLogin() {
                         if (data.user) {
                             setUser(data.user); // 👈 Set the user data
                         }
-                    } catch (error) {
+                    } catch {
+                        
                         setResult('Error submitting data');
                         loginAttemptedRef.current = false;
                     } finally {
